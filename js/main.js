e31b63e833768ee2c73943a8674380bedc42fa60
Carlgo11 <github@carlgo11.com>
2017-06-30 00:59:24 +0200

    Improve phone design & format JS code

--- js/main.js
+++ js/main.js
@@ -1,86 +1,100 @@
-document.getElementById('generate').onclick = genPassword;
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-function genPassword() {
-  var pass = '';
-  for (i = 0; i < document.getElementById('length').value; i++) {
-    var a = [];
-    if (document.getElementById('d').checked) {
-      a.push([
-        48,
-        57
-      ]);
-    }
-    if (document.getElementById('u').checked) {
-      a.push([
-        65,
-        90
-      ]);
-    }
-    if (document.getElementById('l').checked) {
-      a.push([
-        97,
-        122
-      ]);
-    }
-    if (document.getElementById('un').checked) {
-      a.push([
-        161,
-        9999
-      ]);
-    }
-    if (document.getElementById('s').checked) {
-      a.push([
-        33,
-        47
-      ]);
-    }
-    if (document.getElementById('sp').checked) {
-      a.push([
-        32,
-        32
-      ]);
-    }
-    var type = getRandomInt(0, a.length - 1);
-    var min = a[type][0];
-    var max = a[type][1];
-    var randUnicode = String.fromCharCode(getRandomInt(min, max));
-    pass += randUnicode;
-  }
-  document.getElementById('password').value = pass.toString();
-}
-$('#password-button').tooltip({
-  trigger: 'click',
-  placement: 'bottom'
-});
-$('#generate').click(function () {
-  $('#new-password').fadeIn();
-});
-function showTooltip() {
-  $('#password-button').tooltip('show');
-  setTimeout(function () {
-    $('#password-button').tooltip('hide');
-  }, 2000);
-}
-var clipboard = new Clipboard('.btn');
-clipboard.on('success', function (e) {
-  e.clearSelection();
-  showTooltip();
-});
-clipboard.on('error', function (e) {
-  console.log('error copying to clipboard.');
-});
 $(window).load(function() {
-    $('body').fadeIn();
-    $.showLoading({
-	  callback: function() {$.hideLoading();}
-	});
+  $('body').fadeIn();
+  $.showLoading({
+    callback: function() {
+      $.hideLoading();
+    }
+  });
 
 });
-$(document).on("pageload",function(){
-    $.showLoading({
-        name: 'jump-pulse'
-    });
-    console.log("begin");
+$(document).on("pageload", function() {
+  $.showLoading({
+    name: 'jump-pulse'
+  });
+  console.log("begin");
+
+  document.getElementById('generate').onclick = genPassword;
+
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  function genPassword() {
+    var pass = '';
+    for (i = 0; i < document.getElementById('length').value; i++) {
+      var a = [];
+      if (document.getElementById('d').checked) {
+        a.push([
+          48,
+          57
+        ]);
+      }
+      if (document.getElementById('u').checked) {
+        a.push([
+          65,
+          90
+        ]);
+      }
+      if (document.getElementById('l').checked) {
+        a.push([
+          97,
+          122
+        ]);
+      }
+      if (document.getElementById('un').checked) {
+        a.push([
+          161,
+          9999
+        ]);
+      }
+      if (document.getElementById('s').checked) {
+        a.push([
+          33,
+          47
+        ]);
+      }
+      if (document.getElementById('sp').checked) {
+        a.push([
+          32,
+          32
+        ]);
+      }
+      var type = getRandomInt(0, a.length - 1);
+      var min = a[type][0];
+      var max = a[type][1];
+      var randUnicode = String.fromCharCode(getRandomInt(min, max));
+      pass += randUnicode;
+    }
+    document.getElementById('password').value = pass.toString();
+  }
+
+  $('#generate').click(function() {
+    $('#new-password').fadeIn();
+  });
+
+  $('#password-button').tooltip({
+    trigger: 'click',
+    placement: 'bottom'
+  });
+
+  function setTooltip(message) {
+    $('#password-button').tooltip('hide')
+      .attr('data-original-title', message)
+      .tooltip('show');
+  }
+
+  function hideTooltip() {
+    setTimeout(function() {
+      $('#password-button').tooltip('hide');
+    }, 2000);
+  }
+
+  var clipboard = new Clipboard('#password-button');
+  clipboard.on('success', function(e) {
+    e.clearSelection();
+    showTooltip();
+  });
+  clipboard.on('error', function(e) {
+    console.log('error copying to clipboard.');
+  });
 });
