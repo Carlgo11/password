07053850f4e1de7c30818d4aebd84718b756138a
Carlgo11 <Carlgo11@users.noreply.github.com>
2019-06-24 19:51:59 +0200

    Fix bug with selecting from several char type spans. Reported by @kmpoppe

--- js/main.js
+++ js/main.js
@@ -16,65 +16,95 @@ $(document).ready(function () {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
+    function getAvailableChars() {
+        let a = [];
+        /* Numbers */
+        if (document.getElementById('d').checked) {
+            a.push([
+                48,
+                57
+            ]);
+        }
+
+        /* Uppercase letters */
+        if (document.getElementById('u').checked) {
+            a.push([
+                65,
+                90
+            ]);
+        }
+
+        /* Lowercase letters */
+        if (document.getElementById('l').checked) {
+            a.push([
+                97,
+                122
+            ]);
+        }
+
+        /* Unicode characters */
+        if (document.getElementById('un').checked) {
+            a.push([[
+                42128,
+                44031
+            ], [
+                55216,
+                129647
+            ], [
+                192, 8587
+            ], [
+                42240, 42539
+            ]]);
+        }
+
+        /* Special characters */
+        if (document.getElementById('s').checked) {
+            a.push([[
+                33, 47
+            ], [
+                58, 64
+            ], [
+                91, 96
+            ], [
+                123, 126
+            ]]);
+        }
+
+        /* Space */
+        if (document.getElementById('sp').checked) {
+            a.push([
+                32,
+                32
+            ]);
+        }
+        return a;
+    }
+
     function genPassword() {
         let pass = '';
+        const a = getAvailableChars();
 
         for (let i = 0; i < document.getElementById('length').value; i++) {
-            let a = [];
-            if (document.getElementById('d').checked) {
-                a.push([
-                    48,
-                    57
-                ]);
-            }
-            if (document.getElementById('u').checked) {
-                a.push([
-                    65,
-                    90
-                ]);
-            }
-            if (document.getElementById('l').checked) {
-                a.push([
-                    97,
-                    122
-                ]);
-            }
-            if (document.getElementById('un').checked) {
-                a.push([
-                    42128,
-                    44031
-                ], [
-                    55216,
-                    129647
-                ], [
-                    192, 8587
-                ], [
-                    42240, 42539
-                ]);
-            }
-            if (document.getElementById('s').checked) {
-                a.push([
-                    33, 47
-                ], [
-                    58, 64
-                ], [
-                    91, 96
-                ], [
-                    123, 126
-                ]);
-            }
-            if (document.getElementById('sp').checked) {
-                a.push([
-                    32,
-                    32
-                ]);
-            }
+
             const type = getRandomInt(0, a.length - 1);
-            const min = a[type][0];
-            const max = a[type][1];
+            let min;
+            let max;
+            /* Check if a is 2D array */
+            if (a[type][0] !== undefined && a[type][0].constructor === Array) {
+                const charRange = getRandomInt(0, a[type].length - 1);
+                min = a[type][charRange][0];
+                max = a[type][charRange][1];
+            } else {
+                min = a[type][0];
+                max = a[type][1];
+            }
+
+            // console.log(min + "\t" + max);
             const newint = getRandomInt(min, max);
             const newchar = String.fromCharCode(newint);
-            // console.log((1 + pass.length) + ": " + newchar + "\t" + newint); // Debug
+
+            // console.log((1 + pass.length) + ": char:" + newchar + "\t int:" + newint); // Debug
+
             pass += newchar;
         }
 
